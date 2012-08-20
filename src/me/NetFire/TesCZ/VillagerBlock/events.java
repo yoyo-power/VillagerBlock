64081ed550e857bebf792c529c75cc6da8270f84
TesCZ <admin@netfire.cz>
2012-08-20 15:40:10 +0200

    First public version

--- src/me/NetFire/TesCZ/VillagerBlock/events.java
+++ src/me/NetFire/TesCZ/VillagerBlock/events.java
@@ -1,40 +1,150 @@
 package me.NetFire.TesCZ.VillagerBlock;
 
-import org.bukkit.ChatColor;
+import org.bukkit.entity.Arrow;
+import org.bukkit.entity.Egg;
 import org.bukkit.entity.Entity;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.Snowball;
+import org.bukkit.entity.Villager;
+import org.bukkit.entity.Zombie;
+
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByBlockEvent;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
+import org.bukkit.event.entity.PotionSplashEvent;
+import org.bukkit.ChatColor;
 
 public class events implements Listener {
   private final main plugin;
-
+  
   public events(main instance) {
     this.plugin = instance;
   }
-  // todo - kontrola na: *eggs*, splashs, fire, apod-.
-  // + fix nullpointeru
   
+  @EventHandler(priority = EventPriority.HIGH)
+  public void onEntityDamage(EntityDamageEvent ev) {
+	  Entity target=ev.getEntity();
+	  DamageCause type=ev.getCause();
+	  // fire
+	  if(plugin.block_fire && plugin.regions_use && plugin.isProtect(target) && (target instanceof Villager) && (type == DamageCause.FIRE || type == DamageCause.FIRE_TICK)){
+		 if(plugin.clog) plugin.loguj("Some fire tried to hit villager in region on (" + Math.round(target.getLocation().getX()) + "; "+ Math.round(target.getLocation().getY()) + "; "+ Math.round(target.getLocation().getZ()) + "; " + target.getWorld().getName() + ")");
+		 ev.setCancelled(true);
+	  }
+	  if(plugin.block_fire && !plugin.regions_use && (target instanceof Villager) && (type == DamageCause.FIRE || type == DamageCause.FIRE_TICK)){
+		 if(plugin.clog) plugin.loguj("Some fire tried to hit villager on (" + Math.round(target.getLocation().getX()) + "; "+ Math.round(target.getLocation().getY()) + "; "+ Math.round(target.getLocation().getZ()) + "; " + target.getWorld().getName() + ")");
+		 ev.setCancelled(true);
+	  }
+	  // fall
+	  if(plugin.block_fire && plugin.regions_use && plugin.isProtect(target) && (target instanceof Villager) && type == DamageCause.FALL ){
+		 if(plugin.clog) plugin.loguj("Villager tried to fall in region on (" + Math.round(target.getLocation().getX()) + "; "+ Math.round(target.getLocation().getY()) + "; "+ Math.round(target.getLocation().getZ()) + "; " + target.getWorld().getName() + ")");
+		 ev.setCancelled(true);
+	  }
+	  if(plugin.block_fire && !plugin.regions_use && (target instanceof Villager) && type == DamageCause.FALL){
+		 if(plugin.clog) plugin.loguj("Villager tried to fall on (" + Math.round(target.getLocation().getX()) + "; "+ Math.round(target.getLocation().getY()) + "; "+ Math.round(target.getLocation().getZ()) + "; " + target.getWorld().getName() + ")");
+		 ev.setCancelled(true);
+	  }
+  }
+  
+  @EventHandler(priority=EventPriority.HIGH)
+  public void onPotionSplash(PotionSplashEvent event) {
+	  for (LivingEntity et : event.getAffectedEntities()) {
+		  if(et instanceof Villager){
+			 if(plugin.block_potion && plugin.regions_use && plugin.isProtect(et)){
+				 if(plugin.clog) plugin.loguj("Some splash potion tried to hit villager in protect on (" + Math.round(et.getLocation().getX()) + "; "+ Math.round(et.getLocation().getY()) + "; "+ Math.round(et.getLocation().getZ()) + "; " + et.getWorld().getName() + ")");
+				 event.setCancelled(true);
+			 }
+			 if(plugin.block_potion && !plugin.regions_use){
+				 if(plugin.clog) plugin.loguj("Some splash potion tried to hit villager on (" + Math.round(et.getLocation().getX()) + "; "+ Math.round(et.getLocation().getY()) + "; "+ Math.round(et.getLocation().getZ()) + "; " + et.getWorld().getName() + ")");
+				 event.setCancelled(true);
+			 }			  
+		  }
+	  }
+  }
+  
+  @EventHandler(priority=EventPriority.HIGH)
+  public void onEntityDamageByBlock(EntityDamageByBlockEvent ev) {
+      Entity target = ev.getEntity();
+      DamageCause type = ev.getCause();
+	  if(plugin.block_lava && plugin.regions_use && plugin.isProtect(target) && (target instanceof Villager) && type == DamageCause.LAVA ){
+		 if(plugin.clog) plugin.loguj("Lava tried to hit villager in region on (" + Math.round(target.getLocation().getX()) + "; "+ Math.round(target.getLocation().getY()) + "; "+ Math.round(target.getLocation().getZ()) + "; " + target.getWorld().getName() + ")");
+		 ev.setCancelled(true);
+	  }
+	  if(plugin.block_lava && !plugin.regions_use && (target instanceof Villager) && type == DamageCause.LAVA){
+		 if(plugin.clog) plugin.loguj("Lava tried to hit villager on (" + Math.round(target.getLocation().getX()) + "; "+ Math.round(target.getLocation().getY()) + "; "+ Math.round(target.getLocation().getZ()) + "; " + target.getWorld().getName() + ")");
+		 ev.setCancelled(true);
+	  }     
+  }
+	  
   @EventHandler(priority=EventPriority.HIGH)
   public void EntityDamageByEntityEvent(EntityDamageByEntityEvent ev) {
 	Entity player=ev.getDamager();
 	Entity target=ev.getEntity();
-	if((player instanceof Player) && !(target instanceof Player)){
-	  if(target.getType().getName().equalsIgnoreCase("Villager")){
+	if(plugin.block_attack && (player instanceof Player) && !(target instanceof Player)){
+	  if(target instanceof Villager){
 		Player p=(Player) player;
-		if(!p.isOp() && !p.hasPermission("villagerblock.cankill") && !plugin.getWorldGuard().canBuild(p, target.getLocation().getBlock().getRelative(0, 0, 0))){
-		  p.sendMessage(ChatColor.RED + "Zde nemuzes zabijet vesnicany!");
-		  plugin.loguj(p.getName() + " tried to hit villager on (player pos: " + Math.round(p.getLocation().getX()) + "; "+ Math.round(p.getLocation().getY()) + "; "+ Math.round(p.getLocation().getZ()) + "; " + p.getWorld().getName() + "), (villager pos: " + Math.round(target.getLocation().getX()) + "; "+ Math.round(target.getLocation().getY()) + "; "+ Math.round(target.getLocation().getZ()) + "; " + target.getWorld().getName() + ")");
+		if(!p.isOp() && !p.hasPermission("villagerblock.cankill") && plugin.regions_use && !plugin.canPlayerBuild(p, target)){
+		  p.sendMessage(ChatColor.DARK_RED + plugin.message_cantkill);
+		  if(plugin.clog) plugin.loguj(p.getName() + " tried to hit villager in region on (player pos: " + Math.round(p.getLocation().getX()) + "; "+ Math.round(p.getLocation().getY()) + "; "+ Math.round(p.getLocation().getZ()) + "; " + p.getWorld().getName() + "), (villager pos: " + Math.round(target.getLocation().getX()) + "; "+ Math.round(target.getLocation().getY()) + "; "+ Math.round(target.getLocation().getZ()) + "; " + target.getWorld().getName() + ")");
 		  ev.setCancelled(true);
 		}
-	  }
-	}else{
-	  if(target.getType().getName().equalsIgnoreCase("Villager") && (player.getType().getName().equalsIgnoreCase("snowball") || player.getType().getName().equalsIgnoreCase("arrow"))){
-		 plugin.loguj(player.getType().getName() + " tried to hit villager on (villager pos: " + Math.round(target.getLocation().getX()) + "; "+ Math.round(target.getLocation().getY()) + "; "+ Math.round(target.getLocation().getZ()) + "; " + target.getWorld().getName() + ")");
-		 ev.setCancelled(true);
+		if(!plugin.regions_use){
+			if(plugin.clog) plugin.loguj(p.getName() + " tried to hit villager on (player pos: " + Math.round(p.getLocation().getX()) + "; "+ Math.round(p.getLocation().getY()) + "; "+ Math.round(p.getLocation().getZ()) + "; " + p.getWorld().getName() + "), (villager pos: " + Math.round(target.getLocation().getX()) + "; "+ Math.round(target.getLocation().getY()) + "; "+ Math.round(target.getLocation().getZ()) + "; " + target.getWorld().getName() + ")");
+			ev.setCancelled(true);
+		}
 	  }
 	}
-  }
+	if(plugin.block_snowball && (player instanceof Snowball) && !(target instanceof Player)){
+		if(target instanceof Villager){
+			if(plugin.regions_use && plugin.isProtect(target)){
+				if(plugin.clog) plugin.loguj("Some snowball tried to hit villager in region on (" + Math.round(target.getLocation().getX()) + "; "+ Math.round(target.getLocation().getY()) + "; "+ Math.round(target.getLocation().getZ()) + "; " + target.getWorld().getName() + ")");
+				ev.setCancelled(true);
+			}
+			if(!plugin.regions_use){
+				if(plugin.clog) plugin.loguj("Some snowball tried to hit villager on (" + Math.round(target.getLocation().getX()) + "; "+ Math.round(target.getLocation().getY()) + "; "+ Math.round(target.getLocation().getZ()) + "; " + target.getWorld().getName() + ")");
+				ev.setCancelled(true);
+			}
+		}
+	}
+	if(plugin.block_arrow && (player instanceof Arrow) && !(target instanceof Player)){
+		if(target instanceof Villager){
+			if(plugin.regions_use && plugin.isProtect(target)){
+				if(plugin.clog) plugin.loguj("Some arrow tried to hit villager in region on (" + Math.round(target.getLocation().getX()) + "; "+ Math.round(target.getLocation().getY()) + "; "+ Math.round(target.getLocation().getZ()) + "; " + target.getWorld().getName() + ")");
+				ev.setCancelled(true);
+			}
+			if(!plugin.regions_use){
+				if(plugin.clog) plugin.loguj("Some arrow tried to hit villager on (" + Math.round(target.getLocation().getX()) + "; "+ Math.round(target.getLocation().getY()) + "; "+ Math.round(target.getLocation().getZ()) + "; " + target.getWorld().getName() + ")");
+				ev.setCancelled(true);
+			}	
+		}
+	}
+	if(plugin.block_zombies && (player instanceof Zombie) && !(target instanceof Player)){
+		if(target instanceof Villager){
+			if(plugin.regions_use && plugin.isProtect(target)){
+				if(plugin.clog) plugin.loguj("Some zombie tried to hit villager in region on (" + Math.round(target.getLocation().getX()) + "; "+ Math.round(target.getLocation().getY()) + "; "+ Math.round(target.getLocation().getZ()) + "; " + target.getWorld().getName() + ")");
+				ev.setCancelled(true);
+			}
+			if(!plugin.regions_use){
+				if(plugin.clog) plugin.loguj("Some zombie tried to hit villager on (" + Math.round(target.getLocation().getX()) + "; "+ Math.round(target.getLocation().getY()) + "; "+ Math.round(target.getLocation().getZ()) + "; " + target.getWorld().getName() + ")");
+				ev.setCancelled(true);
+			}	
+		}
+	}
+	if(plugin.block_egg && (player instanceof Egg) && !(target instanceof Player)){
+		if(target instanceof Villager){
+			if(plugin.regions_use && plugin.isProtect(target)){
+				if(plugin.clog) plugin.loguj("Some egg tried to hit villager in region on (" + Math.round(target.getLocation().getX()) + "; "+ Math.round(target.getLocation().getY()) + "; "+ Math.round(target.getLocation().getZ()) + "; " + target.getWorld().getName() + ")");
+				ev.setCancelled(true);
+			}
+			if(!plugin.regions_use){
+				if(plugin.clog) plugin.loguj("Some egg tried to hit villager on (" + Math.round(target.getLocation().getX()) + "; "+ Math.round(target.getLocation().getY()) + "; "+ Math.round(target.getLocation().getZ()) + "; " + target.getWorld().getName() + ")");
+				ev.setCancelled(true);
+			}	
+		}
+	}
+  } 
 }
--- src/me/NetFire/TesCZ/VillagerBlock/main.java
+++ src/me/NetFire/TesCZ/VillagerBlock/main.java
@@ -1,13 +1,37 @@
 package me.NetFire.TesCZ.VillagerBlock;
 
 import java.util.logging.Logger;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+
 import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
+import com.sk89q.worldguard.protection.managers.RegionManager;
+import com.sk89q.worldguard.protection.ApplicableRegionSet;
+
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.PluginManager;
 
 public class main extends JavaPlugin {
 	public static Logger log = Logger.getLogger("Minecraft");
+	
+	public Boolean clog = true;
+	public Boolean regions_use = true;
+	public Boolean block_attack = true;
+	public Boolean block_snowball = true;
+	public Boolean block_arrow = true;
+	public Boolean block_potion = true;
+	public Boolean block_zombies = false;
+	public Boolean block_fire = true;
+	public Boolean block_fall = false;
+	public Boolean block_lava = true;
+	public Boolean block_egg = true;	
+	public String regions_plugin = "worldguard";
+	public String message_cantkill = "You don't have permissions for kill a villager.";
 	
 	public void loguj(String what){
 		log.info("[VillagerBlock] " + what);
@@ -15,16 +39,101 @@ public class main extends JavaPlugin {
 	public void onEnable(){
 	    PluginManager pm = getServer().getPluginManager();
 	    pm.registerEvents(new events(this), this);
+	    config_load();
+	    loguj("=================================================");
+	    loguj("Beta version: residences is not supported now :(");
+	    loguj("=================================================");
 		loguj("enabled.");
 	}
 	public void onDisable(){
 		loguj("disabled.");
 	}
-	public WorldGuardPlugin getWorldGuard() {
-	    Plugin plugin = getServer().getPluginManager().getPlugin("WorldGuard");
-	 	    if (plugin == null || !(plugin instanceof WorldGuardPlugin)) {
-	        return null; 
-	    }
-	    return (WorldGuardPlugin) plugin;
-	}
+	
+   public void config_load()
+   {
+     getConfig().addDefault("log", true);
+     getConfig().addDefault("regions.use", true);
+     getConfig().addDefault("regions.plugin", "worldguard");
+     getConfig().addDefault("block.attack", true);
+     getConfig().addDefault("block.snowball", true);
+     getConfig().addDefault("block.arrow", true);
+     getConfig().addDefault("block.potion", true);
+     getConfig().addDefault("block.zombies", false);
+     getConfig().addDefault("block.fire", true);
+     getConfig().addDefault("block.fall", false);
+     getConfig().addDefault("block.lava", true);
+     getConfig().addDefault("block.egg", true);
+     getConfig().addDefault("messages.cantkill", "You don't have permissions for kill a villager.");
+     // add residence support
+     if(!getConfig().getString("regions.plugin").equalsIgnoreCase("worldguard")){
+    	 getConfig().set("regions.plugin", "worldguard");
+    	 getConfig().set("regions.use", false);
+    	 loguj("Plugin '" + getConfig().getString("regions.plugin") + "' is not supported! Regions are now disabled.");
+     }
+     getConfig().options().copyDefaults(true);
+     saveConfig();
+     
+     clog=getConfig().getBoolean("log");
+     regions_use=getConfig().getBoolean("regions.use");
+     regions_plugin=getConfig().getString("regions.plugin");
+     block_attack=getConfig().getBoolean("block.attack");
+     block_snowball=getConfig().getBoolean("block.snowball");
+     block_arrow=getConfig().getBoolean("block.arrow");
+     block_potion=getConfig().getBoolean("block.potion");
+     block_zombies=getConfig().getBoolean("block.zombies");
+     block_fire=getConfig().getBoolean("block.fire");
+     block_fall=getConfig().getBoolean("block.fall");
+     block_lava=getConfig().getBoolean("block.lava");
+     block_egg=getConfig().getBoolean("block.egg");   
+     message_cantkill=getConfig().getString("messages.cantkill");
+  
+     loguj("config loaded.");
+   }
+
+   public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args){	
+		if(cmd.getName().equalsIgnoreCase("vreload")){ 
+			if(sender.isOp() || sender.hasPermission("villagerblock.reload")){
+				config_load();
+				sender.sendMessage(ChatColor.GREEN + "Config reloaded.");
+			}else{
+				sender.sendMessage(ChatColor.DARK_RED + "You don't have permissions.");
+			}		
+			return true;
+		}else{
+			return false;
+		}
+   }
+   
+   public WorldGuardPlugin getWorldGuard() {
+	  Plugin plugin = getServer().getPluginManager().getPlugin("WorldGuard");
+	 	  if (plugin == null || !(plugin instanceof WorldGuardPlugin)) {
+	      return null; 
+	  }
+	  return (WorldGuardPlugin) plugin;
+   }
+   
+   public Boolean canPlayerBuild(Player who, Entity target){
+	  if(regions_plugin.equalsIgnoreCase("worldguard")){
+		  if(!getWorldGuard().canBuild(who, target.getLocation())){
+			  return false;
+		  }
+	  }
+	  if(regions_plugin.equalsIgnoreCase("residence")){
+			  
+	  }
+	  return true;
+   }
+   
+   public Boolean isProtect(Entity target){
+		if(regions_plugin.equalsIgnoreCase("worldguard")){
+			RegionManager r = getWorldGuard().getRegionManager(target.getWorld());
+			com.sk89q.worldedit.Vector v1 = com.sk89q.worldguard.bukkit.BukkitUtil.toVector(target.getLocation());
+			ApplicableRegionSet regions = r.getApplicableRegions(v1);
+			if(regions.size() != 0) return true;
+		}	   
+		if(regions_plugin.equalsIgnoreCase("residence")){
+			  
+		}	   
+		return false;
+   }
 }
